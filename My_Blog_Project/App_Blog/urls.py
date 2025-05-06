2abe213a428e4088cf9aad696a9d7b607fd03fc9
S-rayhan-S <s.rayhansiddikey@gmail.com>
2025-05-06 21:38:42 +0600

    Completed the Write Blog part where users can write & upload blog for public display, in theis section I used class based views like CreateView

--- My_Blog_Project/App_Blog/urls.py
+++ My_Blog_Project/App_Blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.decorators import login_required, permission_required
+
 
 app_name='App_Blog'
 
 urlpatterns = [
-    path('',views.blog_list,name='blog_list')
+    path('',views.blog_list,name='blog_list'),
+    path('write/',login_required(views.CreateBlog.as_view()),name='create_blog')
 ]
 
 
--- My_Blog_Project/App_Blog/views.py
+++ My_Blog_Project/App_Blog/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect,redirect
 
+from .models import Blog,Comment,Likes
+from django.views.generic import ListView,CreateView,UpdateView,DetailView,TemplateView,DeleteView
+from django.urls import reverse,reverse_lazy
+import uuid
 # Create your views here.
 def blog_list(request):
     context={}
     return render(request,'App_Blog/blog_list.html',context=context)
 
+class CreateBlog(CreateView):
+    model=Blog
+    template_name='App_Blog/create_blog.html'
+    fields=['blog_title', 'blog_content']
+    
+    def form_valid(self, form):
+        blog_obj=form.save(commit=False)
+        blog_obj.author=self.request.user
+        title=blog_obj.blog_title
+        blog_obj.slug=title.replace(' ','-')+"-"+str(uuid.uuid4)
+        blog_obj.save()
+        return redirect(reverse('index'))
+        # Here, we would record the user's interest using the message
+        # passed in form.cleaned_data['message']
+        return super().form_valid(form)
 
 
 
